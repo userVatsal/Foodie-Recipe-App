9ec1b7ad8281506104d036a2774cddb5aac447d6
Vatsal Pareshkumar <127995002+userVatsal@users.noreply.github.com>
2025-01-25 16:36:22 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MainFeed from './screens/MainFeed';
+import RecipeDetailScreen from './screens/RecipeDetailScreen';
+import FavoritesScreen from './screens/FavoritesScreen';
+import MyRecipesScreen from './screens/MyRecipesScreen';
+import AddRecipeScreen from './screens/AddRecipeScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="MainFeed">
+        <Stack.Screen name="MainFeed" component={MainFeed} />
+        <Stack.Screen name="RecipeDetail" component={RecipeDetailScreen} />
+        <Stack.Screen name="Favorites" component={FavoritesScreen} />
+        <Stack.Screen name="MyRecipes" component={MyRecipesScreen} />
+        <Stack.Screen name="AddRecipe" component={AddRecipeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
 
