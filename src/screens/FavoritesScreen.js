6c132f6f99e47e554a33ff208f555c965442c968
Vatsal Pareshkumar <127995002+userVatsal@users.noreply.github.com>
2025-01-25 16:39:29 +0000

    Update FavoritesScreen.js

--- src/screens/FavoritesScreen.js
+++ src/screens/FavoritesScreen.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import FavoritesSection from '../components/FavoritesSection';
+
+const FavoritesScreen = ({ favorites }) => {
+  return (
+    <View style={styles.container}>
+      <FavoritesSection favorites={favorites} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+});
+
+export default FavoritesScreen;
 
