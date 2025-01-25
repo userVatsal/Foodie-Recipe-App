63f0643513e5a16ebbe7e99cfa7b1e483be3fbd9
Vatsal Pareshkumar <127995002+userVatsal@users.noreply.github.com>
2025-01-25 16:40:05 +0000

    Update AddRecipeScreen.js

--- src/screens/AddRecipeScreen.js
+++ src/screens/AddRecipeScreen.js
@@ -1,1 +1,25 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import AddRecipeForm from '../components/AddRecipeForm';
+
+const AddRecipeScreen = ({ navigation }) => {
+  const handleSave = (recipe) => {
+    // Save recipe and navigate back
+    navigation.goBack();
+  };
+
+  return (
+    <View style={styles.container}>
+      <AddRecipeForm onSave={handleSave} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+});
+
+export default AddRecipeScreen;
 
