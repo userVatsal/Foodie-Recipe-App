4a5ed8adb990185062c12914aef4ff9caf9a0605
Vatsal Pareshkumar <127995002+userVatsal@users.noreply.github.com>
2025-01-25 16:37:45 +0000

    Update FavoritesSection.js

--- src/components/FavoritesSection.js
+++ src/components/FavoritesSection.js
@@ -1,1 +1,30 @@
+import React from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import RecipeCard from './RecipeCard';
+
+const FavoritesSection = ({ favorites }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Favorites</Text>
+      <FlatList
+        data={favorites}
+        renderItem={({ item }) => <RecipeCard recipe={item} />}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+});
+
+export default FavoritesSection;
 
