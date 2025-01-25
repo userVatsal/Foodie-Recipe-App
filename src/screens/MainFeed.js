9879d24999d5c700851d408b3e5cd004f088fde2
Vatsal Pareshkumar <127995002+userVatsal@users.noreply.github.com>
2025-01-25 16:38:52 +0000

    Update MainFeed.js

--- src/screens/MainFeed.js
+++ src/screens/MainFeed.js
@@ -1,1 +1,37 @@
+import React, { useState } from 'react';
+import { View, FlatList, TouchableOpacity } from 'react-native';
+import RecipeCard from '../components/RecipeCard';
+import CategoryBar from '../components/CategoryBar';
+
+const MainFeed = ({ navigation }) => {
+  const categories = ['Breakfast', 'Lunch', 'Dinner', 'Dessert', 'Snacks', 'Drinks', 'Salads', 'Soups', 'Baking', 'Vegan'];
+  const [recipes, setRecipes] = useState([
+    // Array of recipe objects
+  ]);
+
+  const handleCategoryPress = (category) => {
+    // Filter recipes by category
+  };
+
+  const handleRecipePress = (recipe) => {
+    navigation.navigate('RecipeDetail', { recipe });
+  };
+
+  return (
+    <View>
+      <CategoryBar categories={categories} onCategoryPress={handleCategoryPress} />
+      <FlatList
+        data={recipes}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => handleRecipePress(item)}>
+            <RecipeCard recipe={item} />
+          </TouchableOpacity>
+        )}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </View>
+  );
+};
+
+export default MainFeed;
 
